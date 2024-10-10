8cc834ef192ef9d60478a583f5956f67db798303
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-10 17:09:38 +0900

    feat: cursor 방식 pagination 조회 jpql 작성

--- src/main/java/com/example/review/review/repository/ReviewRepository.java
+++ src/main/java/com/example/review/review/repository/ReviewRepository.java
@@ -1,6 +1,10 @@
 package com.example.review.review.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.review.review.domain.Review;
@@ -8,4 +12,7 @@ import com.example.review.review.domain.Review;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 	boolean existsByUserIdAndProductId(Long userId, Long productId);
+
+	@Query("select r from Review r where r.product.id = :productId and r.id > :cursor")
+	List<Review> findAllByProductIdPagination(Long productId, Long cursor, Pageable pageable);
 }
