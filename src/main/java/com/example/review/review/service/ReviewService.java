ce3d8b38e2875a9b9c97a9ca8605f428a0ce6449
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-10 17:10:07 +0900

    feat: 리뷰 목록 조회 로직 구현

--- src/main/java/com/example/review/review/service/ReviewService.java
+++ src/main/java/com/example/review/review/service/ReviewService.java
@@ -1,7 +1,9 @@
 package com.example.review.review.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +13,8 @@ import com.example.review.product.domain.Product;
 import com.example.review.product.repository.ProductRepository;
 import com.example.review.review.domain.Review;
 import com.example.review.review.dto.request.CreateReviewDto;
+import com.example.review.review.dto.response.FindReviewPageDto;
+import com.example.review.review.dto.response.ReviewResponseDto;
 import com.example.review.review.repository.ReviewRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -37,6 +41,27 @@ public class ReviewService {
 		reviewRepository.save(review);
 
 		updateProductAvgScoreAndCount(findProduct, reviewDto.score());
+	}
+
+	@Transactional(readOnly = true)
+	public FindReviewPageDto findAllReviews(Long productId, Long cursor, Pageable pageable) {
+		Product findProduct = findById(productId);
+		List<Review> reviewList = reviewRepository.findAllByProductIdPagination(productId, cursor, pageable);
+		List<ReviewResponseDto> reviewResponseDtos = convertToReviewResponseDto(reviewList);
+
+		return createFindReviewPageDto(cursor, findProduct, reviewResponseDtos);
+	}
+
+	private FindReviewPageDto createFindReviewPageDto(Long cursor, Product findProduct,
+		List<ReviewResponseDto> reviewResponseDtos) {
+		return new FindReviewPageDto(findProduct.getReviewCount(), findProduct.getAvgScore(), cursor,
+			reviewResponseDtos);
+	}
+
+	private static List<ReviewResponseDto> convertToReviewResponseDto(List<Review> reviewList) {
+		return reviewList.stream()
+			.map(ReviewResponseDto::new)
+			.toList();
 	}
 
 	private void updateProductAvgScoreAndCount(Product product, int reviewScore) {
