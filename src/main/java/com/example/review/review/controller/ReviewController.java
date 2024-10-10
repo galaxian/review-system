10e11edc4289733da8758710bf99b204386f7986
Lee <94841127+galaxian@users.noreply.github.com>
2024-10-10 17:10:53 +0900

    feat: 리뷰 조회 api controller 구현

--- src/main/java/com/example/review/review/controller/ReviewController.java
+++ src/main/java/com/example/review/review/controller/ReviewController.java
@@ -1,12 +1,18 @@
 package com.example.review.review.controller;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.review.review.dto.request.CreateReviewDto;
+import com.example.review.review.dto.response.FindReviewPageDto;
 import com.example.review.review.service.ReviewService;
 
 import jakarta.validation.Valid;
@@ -24,5 +30,13 @@ public class ReviewController {
 		@RequestPart(value = "imgFile", required = false)
 		MultipartFile imageFile) {
 		reviewService.CreateReview(reviewDto, productId, imageFile);
+	}
+
+	@GetMapping("/products/{productId}/reviews")
+	public ResponseEntity<FindReviewPageDto> findAllReviewPage(@PathVariable("productId") Long productId,
+		@RequestParam(name = "cursor", defaultValue = "0") Long cursor,
+		@RequestParam(name = "size", defaultValue = "10") int size) {
+		Pageable pageable = PageRequest.of(0, size);
+		return ResponseEntity.ok(reviewService.findAllReviews(productId, cursor, pageable));
 	}
 }
